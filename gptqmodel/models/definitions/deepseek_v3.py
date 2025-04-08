ec90dd0ccb2c8a6b7ea2b3fd75ad137e50bbb3e9
Qubitium <Qubitium@modelcloud.ai>
2025-04-08 07:30:12 +0000

    typo
    Signed-off-by: Qubitium <Qubitium@modelcloud.ai>

--- gptqmodel/models/definitions/deepseek_v3.py
+++ gptqmodel/models/definitions/deepseek_v3.py
@@ -38,7 +38,7 @@ class DeepSeekV3GPTQ(BaseGPTQModel):
     layer_modules_strict = False
 
     layer_modules = [
-        ["self_attn.q_a_proj", "self_attn.kv_a_proj_with_mqa"]
+        ["self_attn.q_a_proj", "self_attn.kv_a_proj_with_mqa"],
         ["self_attn.q_b_proj", "self_attn.kv_b_proj"],
         ["self_attn.o_proj"],
 
